ad43aecc03577060ae9640b89de07e9c66ed43c6
Manilka Keshan Abeygunaratna <141556672+MKabeygunaratna@users.noreply.github.com>
2025-02-02 23:56:49 +0530

    Delete lib directory

--- lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xFF7261C6);
-const Color backgroundColor = Color(0xFFD3CBFD);
-// You can add more constants or styles here.

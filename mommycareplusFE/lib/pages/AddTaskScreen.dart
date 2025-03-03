ac559f9c5edf9861a7edcf7bcc103f7307bb34d6
NotSithuwa <ishamyafernando@gmail.com>
2025-03-04 02:08:56 +0530

    Update AddTaskScreen

--- mommycareplusFE/lib/pages/AddTaskScreen.dart
+++ mommycareplusFE/lib/pages/AddTaskScreen.dart
@@ -4,8 +4,34 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Add Task Screen"),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Add Task'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Topic"),
+            TextField(),
+            SizedBox(height: 16),
+            Text("Description"),
+            TextField(maxLines: 4),
+            SizedBox(height: 16),
+            Text("Date"),
+            TextField(),
+            SizedBox(height: 16),
+            Text("Time"),
+            TextField(),
+          ],
+        ),
       ),
     );
   }

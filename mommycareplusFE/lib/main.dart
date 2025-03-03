99b39463ebe8ef93e557e85cc4bec56be38d1cff
Manilka Keshan Abeygunaratna <manilkakeshan1000@gmail.com>
2025-03-03 21:36:52 +0530

    debug disabled

--- mommycareplusFE/lib/main.dart
+++ mommycareplusFE/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:mommycareplusFE/pages/signup.dart';
 
 // import 'package:login/pages/EmailVerification.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -18,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,

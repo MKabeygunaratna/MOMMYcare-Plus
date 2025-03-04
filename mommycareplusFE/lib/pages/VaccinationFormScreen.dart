1ae6ceb89acf70bac43d094f13c03c18f86aefea
NotSithuwa <ishamyafernando@gmail.com>
2025-03-04 11:18:35 +0530

    Vaccination Inputs

--- mommycareplusFE/lib/pages/VaccinationFormScreen.dart
+++ mommycareplusFE/lib/pages/VaccinationFormScreen.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(VaccinationFormApp());
-}
-
-class VaccinationFormApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: VaccinationFormScreen(),
-    );
-  }
-}
-
 class VaccinationFormScreen extends StatelessWidget {
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController vaccineController = TextEditingController();
+  final TextEditingController dateController = TextEditingController();
+  final TextEditingController batchController = TextEditingController();
+  final TextEditingController effectsController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Vaccination Form Screen"),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {},
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Age"),
+            TextField(controller: ageController),
+            Text("Type of vaccine"),
+            TextField(controller: vaccineController),
+            Text("Date"),
+            TextField(controller: dateController),
+            Text("Batch No"),
+            TextField(controller: batchController),
+            Text("Adverse effects"),
+            TextField(controller: effectsController),
+          ],
+        ),
       ),
     );
   }

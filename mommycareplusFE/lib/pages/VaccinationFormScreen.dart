c11e23762659fead8b27c0089ba2ce011ac9f2c9
NotSithuwa <ishamyafernando@gmail.com>
2025-03-04 11:50:04 +0530

    Updated Vaccination Form

--- mommycareplusFE/lib/pages/VaccinationFormScreen.dart
+++ mommycareplusFE/lib/pages/VaccinationFormScreen.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(VaccinationFormApp());
+}
+
+class VaccinationFormApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: VaccinationFormScreen(),
+    );
+  }
+}
+
 class VaccinationFormScreen extends StatelessWidget {
   final TextEditingController ageController = TextEditingController();
   final TextEditingController vaccineController = TextEditingController();
@@ -23,19 +37,62 @@ class VaccinationFormScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Age"),
-            TextField(controller: ageController),
-            Text("Type of vaccine"),
-            TextField(controller: vaccineController),
-            Text("Date"),
-            TextField(controller: dateController),
-            Text("Batch No"),
-            TextField(controller: batchController),
-            Text("Adverse effects"),
-            TextField(controller: effectsController),
+            Center(
+              child: Text(
+                'Vaccination Records',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20),
+            _buildTextField('Age', ageController),
+            _buildTextField('Type of vaccine', vaccineController),
+            _buildTextField('Date', dateController),
+            _buildTextField('Batch No', batchController),
+            _buildTextField('Adverse effects', effectsController),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Add',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.purple),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.purple),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
     );
   }
 }

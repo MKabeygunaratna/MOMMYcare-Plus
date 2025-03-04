bddaeef13d38c4d3ae6240386aa3254b95d20049
Monali Rathnayake <164992420+monali0218@users.noreply.github.com>
2025-03-04 12:20:32 +0530

    added a recently watched screen

--- mommycareplusFE/lib/pages/videoPlayerScreen.dart
+++ mommycareplusFE/lib/pages/videoPlayerScreen.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoPlayerScreen extends StatelessWidget {
-  final String videoUrl;
+class VideoPlayerScreen extends StatefulWidget {
+  final String videoId;
+  final String title;
 
-  const VideoPlayerScreen({super.key, required this.videoUrl});
+  VideoPlayerScreen({required this.videoId, required this.title});
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoId,
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Playing Video")),
-      body: Center(child: Text("Playing video from URL: $videoUrl")),
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.purple,
+      ),
+      body: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }

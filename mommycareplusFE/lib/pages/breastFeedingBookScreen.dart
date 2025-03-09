3af8bc23095206d2f458ad1c94bc551ac314f4ba
Monali Rathnayake <164992420+monali0218@users.noreply.github.com>
2025-03-10 01:23:57 +0530

    book details

--- mommycareplusFE/lib/pages/breastFeedingBookScreen.dart
+++ mommycareplusFE/lib/pages/breastFeedingBookScreen.dart
@@ -8,7 +8,7 @@ class Breastfeedingbookscreen extends StatelessWidget {
       imageUrl: 'assets/The Womanly Art of Breastfeeding.jpeg',
       description: '''
 
-**Laid-back Breastfeeding** 
+" **Laid-back Breastfeeding** 
  laid- back breastfeeding, or Biological Nurturing, means getting
 comfortable with your baby and encouraging your own and your baby’s
 natural breastfeeding instincts. See biologicalnurturing.com for further
@@ -73,7 +73,7 @@ sign of hunger. Calm her down before trying to feed her.
 for her demands.
  • Don’t wait for your breasts to feel full. A full breast has already started
 to slow down production.
- • Offer even if she’s not asking, anytime you like.
+ • Offer even if she’s not asking, anytime you like."
       ''',
     ),
 
@@ -99,23 +99,25 @@ Complete with age-appropriate strategies for dealing with day-to-day struggles a
     ),
 
     Book(
-      author: 'Daniel J. Siegel and Tina Payne Bryson',
-      title: 'The Whole-Brain Child: 12 Revolutionary Strategies to Nurture Your Childs Developing Mind',
-      imageUrl: 'assets/lord_of_the_rings.jpg',
+      author: 'Ina May Gaskin',
+      title: '''Ina May's Guide to Breastfeeding''',
+      imageUrl: 'assets/Ina May Gaskin.jpg',
       description: '''
-"Simple, smart, and effective solutions to your child’s struggles.”—Harvey Karp, M.D.
+"Everything you need to know to make breastfeeding a joyful, natural, and richly fulfilling experience for both you and your baby
 
-In this pioneering, practical book, Daniel J. Siegel, neuropsychiatrist and author of the bestselling Mindsight, and parenting expert Tina Payne Bryson offer a revolutionary approach to child rearing with twelve key strategies that foster healthy brain development, leading to calmer, happier children. The authors explain—and make accessible—the new science of how a child’s brain is wired and how it matures. The “upstairs brain,” which makes decisions and balances emotions, is under construction until the mid-twenties. And especially in young children, the right brain and its emotions tend to rule over the logic of the left brain. No wonder kids throw tantrums, fight, or sulk in silence. By applying these discoveries to everyday parenting, you can turn any outburst, argument, or fear into a chance to integrate your child’s brain and foster vital growth.
+Drawing on her decades of experience in caring for pregnant women, mothers, and babies, Ina May Gaskin explores the health and psychological benefits of breastfeeding and gives you invaluable practical advice that will help you nurse your baby in the most fulfilling way possible. Inside you’ll find answers to virtually every question you have on breastfeeding, including topics such as
+•the benefits of breastfeeding
+•nursing challenges
+•pumps and other nursing products
+•sleeping arrangements
+•nursing and work
+•medications
+•nursing multiples
+•weaning
+•sick babies
+•nipplephobia, and much more
 
-Complete with age-appropriate strategies for dealing with day-to-day struggles and illustrations that will help you explain these concepts to your child, The Whole-Brain Child shows you how to cultivate healthy emotional and intellectual development so that your children can lead balanced, meaningful, and connected lives.
-
-“[A] useful child-rearing resource for the entire family . . . The authors include a fair amount of brain science, but they present it for both adult and child audiences.”—Kirkus Reviews
-
-“Strategies for getting a youngster to chill out [with] compassion.”—The Washington Post
-
-“This erudite, tender, and funny book is filled with fresh ideas based on the latest neuroscience research. I urge all parents who want kind, happy, and emotionally healthy kids to read The Whole-Brain Child. This is my new baby gift.”—Mary Pipher, Ph.D., author of Reviving Ophelia and The Shelter of Each Other
-
-“Gives parents and teachers ideas to get all parts of a healthy child’s brain working together.”—Parent to Parent''',
+Ina May's Guide to Breastfeeding is filled with helpful advice, medical facts, and real-life stories that will help you understand how and why breastfeeding works and how you can use it to more deeply connect with your baby and your own body. Whether you’re planning to nurse for the first time or are looking for the latest, most up-to-date expert advice available, you couldn’t hope to find a better guide than Ina May."''',
     ),
 
     Book(

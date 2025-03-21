64773e22db5ffac728c0398dfa53210f7edafe89
Yeheni2003 <yehenivishmini@icloud.com>
2025-03-22 00:50:54 +0530

    adding shared preferences

--- mommycareplusFE/lib/pages/UserProvider.dart
+++ mommycareplusFE/lib/pages/UserProvider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider extends ChangeNotifier {
   String _name = "";
@@ -9,18 +10,40 @@ class UserProvider extends ChangeNotifier {
   String get age => _age;
   String get postpartum => _postpartum;
 
+  UserProvider(){
+    _loadFromPrefs();
+  }
+
+  Future<void>_loadFromPrefs()async{
+    final prefs = await SharedPreferences.getInstance();
+    _name = prefs.getString('userName')??'';
+    _age = prefs.getString('userAge')??'';
+    _postpartum = prefs.getString('userPostpartumPeriod')??'';
+    notifyListeners();
+  }
+
+  Future<void>_saveToPrefs()async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('userName', _name);
+    await prefs.setString('userAge', _age);
+    await prefs.setString('userPostpartumPeriod', _postpartum);
+  }
+
   void setName(String name) {
     _name = name;
+    _saveToPrefs();
     notifyListeners();
   }
 
   void setAge(String age) {
     _age = age;
+    _saveToPrefs();
     notifyListeners();
   }
 
   void setPostpartum(String postpartum) {
     _postpartum = postpartum;
+    _saveToPrefs();
     notifyListeners();
   }
 
@@ -32,6 +55,7 @@ class UserProvider extends ChangeNotifier {
     _name = name;
     _age = age;
     _postpartum = postpartum;
+    _saveToPrefs();
     notifyListeners();
   }
 }

8993b17585cea154d502bcb117032e4951373c55
Yeheni2003 <yehenivishmini@icloud.com>
2025-03-21 15:04:50 +0530

    adding changes to the home screen

--- mommycareplusFE/lib/pages/home.dart
+++ mommycareplusFE/lib/pages/home.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
-
+//import 'package:mommycareplusFE/pages/ChatScreen.dart';
+import 'package:mommycareplusFE/pages/ProfileScreen.dart';
+import 'package:mommycareplusFE/pages/Question_welcome_page.dart';
+import 'package:mommycareplusFE/pages/Questions.dart';
+import 'package:mommycareplusFE/pages/SettingsScreen.dart';
+import 'package:mommycareplusFE/pages/SubscriptionScreen.dart';
+import 'package:mommycareplusFE/pages/UserProvider.dart';
+import 'package:mommycareplusFE/pages/chatbot1.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,10 +23,35 @@ class _HomePageState extends State<HomePage> {
   final List<String> _tasks = [];
 
   void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-    // TODO: Implement navigation to respective screens
+    if(index==_currentIndex)
+      return;
+
+    switch(index){
+      case 0:
+        Navigator.pushReplacement(
+            context,MaterialPageRoute(builder: (context)=>HomePage()));
+        break;
+      case 1:
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context)=>ChatbotScreen())
+        );
+        break;
+      case 2:
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context)=>ChatbotScreen())
+        );
+      case 3:
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context)=>ProfileScreen())
+        );
+
+
+
+
+    }
   }
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
@@ -26,6 +59,7 @@ class _HomePageState extends State<HomePage> {
       _selectedDay = selectedDay;
       _focusedDay = focusedDay;
     });
+
 
     if (selectedDay.isAfter(DateTime.now())) {
       _addReminder(selectedDay);
@@ -121,26 +155,54 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final userName = Provider.of<UserProvider>(context).name;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello $userName!"),
+        actions: [
+          /**IconButton(
+            icon: Icon(Icons.chat,color: Colors.black),
+            tooltip: "Mothers' Chat",
+            onPressed: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context)=> ChatScreen())
+              );
+            },
+          ),**/
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.black),
+            tooltip: "Settings",
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=> SettingsScreen()),
+              );
+            },
+          )
+        ],
+      ),
       backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: _onTabTapped,
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset('assets/images/home/home.png', width: 24, height: 24),
+            icon: Image.asset('assets/images/home.png', width: 24, height: 24),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/images/home/robot.png', width: 24, height: 24),
+            icon: Image.asset('assets/images/robot.png', width: 24, height: 24),
             label: 'AI',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/images/home/book.png', width: 24, height: 24),
+            icon: Image.asset('assets/images/book.png', width: 24, height: 24),
             label: 'Resources',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/images/home/Profile.png', width: 24, height: 24),
+            icon: Image.asset('assets/images/Profile.png', width: 24, height: 24),
             label: 'Profile',
           ),
         ],
@@ -150,24 +212,22 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: screenHeight * 0.02),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Hello User',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
-              ),
-              SizedBox(height: 16),
-              _buildMotivationCard(),
-              SizedBox(height: 16),
-              _buildAssessmentCard(),
-              SizedBox(height: 16),
+              SizedBox(height: screenHeight * 0.02),
+              _buildMotivationCard(screenWidth, screenHeight),
+              SizedBox(height: screenHeight * 0.02),
+              _buildAssessmentCard(screenWidth),
+              SizedBox(height: screenHeight * 0.02),
               _buildCalendar(),
-              SizedBox(height: 16),
-              _buildAssistantCard(),
-              SizedBox(height: 16),
-              _buildTasksSection(),
+              SizedBox(height: screenHeight * 0.02),
+              _buildAssistantCard(screenWidth),
+              SizedBox(height: screenHeight * 0.02),
+              _buildUpgradeSection(screenWidth),
+              SizedBox(height: screenHeight * 0.02),
+              _buildTasksSection(screenWidth,screenHeight),
             ],
           ),
         ),
@@ -175,17 +235,17 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildMotivationCard() {
+  Widget _buildMotivationCard(double screenWidth, double screenHeight) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         image: DecorationImage(
-          image: AssetImage('assets/images/home/image1.png'),
+          image: AssetImage('assets/images/image1.png'),
           fit: BoxFit.cover,
         ),
       ),
-      height: 150,
-      padding: EdgeInsets.all(16),
+      height: screenHeight * 0.2,
+      padding: EdgeInsets.all(screenWidth * 0.04),
       alignment: Alignment.bottomLeft,
       child: Text(
         'Some days are harder than others but remember, it gets easier with time.',
@@ -194,91 +254,140 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildAssessmentCard() {
+  Widget _buildAssessmentCard(double screenWidth) {
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(screenWidth * 0.04),
       decoration: _buildBoxDecoration(),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             'How are you feeling today?',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+            style: TextStyle(fontSize: screenWidth * 0.05, fontWeight: FontWeight.bold, color: Colors.black),
           ),
           SizedBox(height: 16),
           Text(
             'Take this quick, 10-question self-assessment to check your emotional well-being.',
-            style: TextStyle(fontSize: 11, color: Colors.black),
+            style: TextStyle(fontSize: screenWidth * 0.03, color: Colors.black),
           ),
           SizedBox(height: 16),
           ElevatedButton(
-            onPressed: () {}, // TODO: Add navigation to assessment page
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=>QuestionWelcomePage()),
+              );
+            }, // TODO: Add navigation to assessment page
             style: _buttonStyle(),
-            child: Text('Start Assessment', style: TextStyle(color: Colors.white)),
+            child: Text('Start Assessment', style: TextStyle(fontSize: screenWidth * 0.04, color: Colors.white)),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildAssistantCard() {
+  Widget _buildAssistantCard(double screenWidth) {
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(screenWidth * 0.04),
       decoration: BoxDecoration(
         color: Colors.white,
+        borderRadius: BorderRadius.circular(8), // Optional: Adds rounded corners to the card
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          // Image with flexible space
           Image.asset(
-            'assets/images/home/image2.png',
-            width: 85, // Increased size
+            'assets/images/image2.png',
+            width: screenWidth * 0.2, // Adjusted size for responsiveness
           ),
-          SizedBox(width: 5),
-          Container(
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color(0xFF7261C6),
-                width: 2,
+          SizedBox(width: screenWidth * 0.02),
+          // Wrap content in Expanded to avoid overflow
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(screenWidth * 0.03),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color(0xFF7261C6),
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(8),
               ),
-              borderRadius: BorderRadius.circular(8), // Optional: Rounded corners
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Hi User!! How can I help you?',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(height: 6), // Added space
-                Text(
-                  'I can answer any of your questions\n'
-                      'related to postpartum and\n pregnancy.'
-                      'Just ask here.',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black87,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Row(
-                  children: [
-                    SizedBox(width: 80),
-                    ElevatedButton(
-                      onPressed: () {}, // TODO: Add AI assistant logic
-                      style: _buttonStyle(),
-                      child: Text(
-                        'Get Started',
-                        style: TextStyle(color: Colors.white),
-                      ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hi User!! How can I help you?',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(height: screenWidth * 0.02),
+                  Text(
+                    'I can answer any of your questions\nrelated to postpartum and\npregnancy.\nJust ask here.',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: screenWidth * 0.02),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context)=>ChatbotScreen()),
+                          );
+                        }, // TODO: Add AI assistant logic
+                        style: _buttonStyle(),
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUpgradeSection(double screenWidth) {
+    return Container(
+      width: screenWidth * 0.9,
+      margin: EdgeInsets.symmetric(vertical: 16),
+      padding: EdgeInsets.all(screenWidth * 0.05),
+      decoration: BoxDecoration(
+        color: Color(0xFFD3CBFD),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Upgrade to Premium',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context)=>SubscriptionPlan())
+              );
+            },
+            style: _buttonStyle(),
+            child: Text(
+              'Upgrade',
+              style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ),
         ],
@@ -287,7 +396,7 @@ class _HomePageState extends State<HomePage> {
   }
 
 
-  Widget _buildTasksSection() {
+  Widget _buildTasksSection(double screenWidth, double screenHeight) {
     TextEditingController _taskController = TextEditingController(); // Controller for the task input
 
     return Column(
@@ -298,7 +407,7 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(
               fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
         ),
-        SizedBox(height: 8),
+        SizedBox(height: screenHeight * 0.01),
         GestureDetector(
           onTap: () {
             // Show dialog to input new task
@@ -331,7 +440,7 @@ class _HomePageState extends State<HomePage> {
             );
           },
           child: Container(
-            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            padding: EdgeInsets.all(screenWidth * 0.03),
             decoration: BoxDecoration(
               color: Color(0xFF7261C6),
               borderRadius: BorderRadius.circular(16),
@@ -342,11 +451,11 @@ class _HomePageState extends State<HomePage> {
                   'Add a new task ',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 14,
+                    fontSize: screenWidth * 0.04,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(width: 8),
+                SizedBox(width: screenWidth * 0.02),
                 Icon(Icons.add, color: Colors.white),
               ],
             ),
@@ -390,5 +499,4 @@ class _HomePageState extends State<HomePage> {
   ButtonStyle _buttonStyle() {
     return ElevatedButton.styleFrom(backgroundColor: Color(0xFF7261C6), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)));
   }
-
 }
